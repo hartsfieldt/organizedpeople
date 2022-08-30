fb76ed6c31a9ff03a4a3a2a153bbbb66874bff36
Your Name <you@example.com>
2022-08-30 12:42:33 -0400

    updated the text area

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 var currentDay = document.getElementById("currentDay");
-
+var currentTime = document.getElementById("hour");
+var hourBlock = document.getElementById("time-block");
+var taskList = document.querySelector("description");
+var tasks = [];
 
 var todaysDate = moment();
 currentDay.textContent = todaysDate.format("LLLL");
+
+
+$( ".btn" ).click(function() {
+    console.log(this);
+    console.log($(this).siblings("textarea"));
+    console.log($(this).siblings("textarea").val());
+    console.log($(this).siblings("textarea").attr('id'));
+
+    var taskValue = $(this).siblings("textarea").val();
+    var taskId = $(this).siblings("textarea").attr('id');
+    localStorage.setItem(taskId, taskValue);
+
+    var savedTaskValue = localStorage.getItem(taskValue);
+    var savedTaskId = localStorage.getItem(taskId);
+    localStorage.getItem(savedTaskValue, savedTaskId, JSON.stringify(taskValue))
+  });
+
+
+
